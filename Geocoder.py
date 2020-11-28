ac6caa827d68d3afbb916db4486c9b5aa1b8821e
dussn <75049277+dussn@users.noreply.github.com>
2020-11-27 22:19:43 -0500

    Add files via upload

--- Geocoder.py
+++ Geocoder.py
@@ -4,11 +4,11 @@ import json
 base_url = 'http://api.zippopotam.us/us/'
 
 city = input("Enter a City:  ")
-state = inpuit("Enter a State: ")
+state = input("Enter a State: ")
 
 
 
-r = requests.get(state.lower()+"/"+city.lower())
+r = requests.get(base_url+state.lower()+"/"+city.lower())
 j = r.json()
 try:
     coords = (float(j["places"][0]["latitude"]), float(j["places"][0]["longitude"]))
